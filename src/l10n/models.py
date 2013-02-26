46b2da0bb81ff416b4a498c517a6d7ce9bd3fa4f
asimong <asimong@gmail.com>
2013-02-26 14:35:07 +0000

    #862355

--- src/l10n/models.py
+++ src/l10n/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Language(models.Model):
     code = models.CharField(primary_key=True, max_length=32) #e.g. en-gb, en-us, en-au, nl-nl, nl-nl, pt-pt, pt-br
     en_name = models.CharField(max_length=255) #e.g. Spanish
-    loc_name = models.CharField(max_length=255) #e.g. Castellano
+    loc_name = models.CharField(max_length=255) #e.g. Castellano or Mexican
 
     def __unicode__(self):
         return u'%s: %s ( %s )' % (self.code, self.loc_name, self.en_name)
