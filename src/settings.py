d52ecb609e2282d4bd7c19d936cc54e31c223b30
Henk Vos <henk.vos@me.com>
2013-04-11 22:33:45 +0200

    added files thru collectstaic: admingenericadmingrappelliiconsjsrest_framework

--- src/settings.py
+++ src/settings.py
@@ -67,7 +67,7 @@ MEDIA_URL = 'http://dbe6thbrwufu9.cloudfront.net/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'collectstatic/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -78,7 +78,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/Users/Henk/InLOC/src/static/',
+    '/Users/henkvos/InLOC/src/static/',
+	#'/Users/henkvos/InLOC/src/static/',
 )
 
 # List of finder classes that know how to find static files in
