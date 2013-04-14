14cbbbc4d022ee8d815d592421bce02e9cfb0bd5
Henk Vos <henk.vos@me.com>
2013-04-15 00:47:46 +0200

    #929208

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls import patterns, include, url
 
 from api.views import api_root
 from l10n.views import LanguageList
-from loc.views import LOCStructureList, LOCStructureDetail, LOCSearch
+from loc.views import LOCStructureList, LOCStructureDetail, LOCSearch, Search
 
 
 
 urlpatterns = patterns('',
                        url(r'^$', api_root),
-                       url(r'^search$', LOCSearch.as_view(), name='search'),
+                       url(r'^search$', Search.as_view(), name='search'),
+                       url(r'^locsearch$', LOCSearch.as_view(), name='search'),
                        url(r'^languages/$', LanguageList.as_view(), name='language-list'),
                        url(r'^locstructures/(?P<pk>[0-9]+)$', LOCStructureDetail.as_view(), name='locstructure-detail'),
                        url(r'^locstructures/$', LOCStructureList.as_view(), name='locstructure-list'),
--- src/urls.py
+++ src/urls.py
@@ -6,8 +6,9 @@ from django.contrib import admin
 from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
 
-from auth.views import LogOut
+from auth.views import LogOut, LogIn
 from home.views import Home
+from loc.views import LOCDefitionView, LOCStructureView
 from utils.views import XMLImportView
 
 admin.autodiscover()
@@ -16,6 +17,8 @@ urlpatterns = patterns('',
 
 
     (r'^api/', include('api.urls')),
+    (r'^view/locdefinition/(?P<id>[0-9]+)/$', LOCDefitionView.as_view()),
+    (r'^view/locstructure/(?P<id>[0-9]+)/$', LOCStructureView.as_view()),
 
     (r'^grappelli/', include('grappelli.urls')),
     (r'^partials/', include('partials.urls')),
@@ -23,6 +26,7 @@ urlpatterns = patterns('',
     (r'^import/', XMLImportView.as_view()),
 
     (r'^logout/', LogOut.as_view()),
+    (r'^login/', LogIn.as_view()),
     (r'^admin/logout/', LogOut.as_view()),
 
     url(r'^admin/', include(admin.site.urls)),
